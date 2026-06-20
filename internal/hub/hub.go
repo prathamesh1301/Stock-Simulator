a2d8382c5d3511da5d2246efe94304fb4f75e09e
prathamesh1301 <prathameshautade107@gmail.com>
2026-06-20 10:45:45 +0530

    feat: add binance reconnection logic

--- internal/hub/hub.go
+++ internal/hub/hub.go
@@ -9,92 +9,71 @@ import (
 
 type Hub struct {
 	Clients map[*domain.Client]bool
-
 	Register   chan *domain.Client
 	Unregister chan *domain.Client
-
 	Broadcast     chan domain.MarketEvent
 	Subscriptions map[string]map[*domain.Client]bool
-	SymbolCounts map[string]int
-	FeedCommands chan domain.FeedCommand
+	SymbolCounts  map[string]int
+	FeedCommands  chan domain.FeedCommand
 }
 
 func NewHub() *Hub {
 
 	return &Hub{
 		Clients: make(map[*domain.Client]bool),
-
 		Register:   make(chan *domain.Client),
 		Unregister: make(chan *domain.Client),
-
 		Broadcast:     make(chan domain.MarketEvent),
 		Subscriptions: make(map[string]map[*domain.Client]bool),
-		SymbolCounts: make(map[string]int),
-		FeedCommands: make(chan domain.FeedCommand),
+		SymbolCounts:  make(map[string]int),
+		FeedCommands:  make(chan domain.FeedCommand),
 	}
 }
 
-func (h *Hub) Run(ctx context.Context,wg *sync.WaitGroup,feedCommands chan domain.FeedCommand) {
+func (h *Hub) Run(ctx context.Context, wg *sync.WaitGroup, feedCommands chan domain.FeedCommand) {
 	defer wg.Done()
 	for {
 
 		select {
-			case <-ctx.Done():
-				fmt.Println("Stopping hub")
-				return
+		case <-ctx.Done():
+			fmt.Println("Stopping hub")
+			return
 
 		case client := <-h.Register:
-
 			h.Clients[client] = true
-
 			fmt.Println("Client registered")
 
 		case client := <-h.Unregister:
 
 			if _, ok := h.Clients[client]; ok {
-
 				for symbol := range client.Subscriptions {
-
 					if subscribers, ok := h.Subscriptions[symbol]; ok {
-
 						delete(subscribers, client)
-
 						if len(subscribers) == 0 {
 							delete(h.Subscriptions, symbol)
 						}
 					}
 				}
-
 				delete(h.Clients, client)
-
 				close(client.Send)
-
 				client.Conn.Close()
-
 				fmt.Println("Client unregistered")
 			}
 
 		case event := <-h.Broadcast:
-
 			subscribers, ok := h.Subscriptions[event.StockName]
-
 			if !ok {
 				continue
 			}
-
 			for client := range subscribers {
-
 				select {
-
 				case client.Send <- event.Data:
-
 				default:
-
 					h.Unregister <- client
 				}
 			}
-		case feed:= <-h.FeedCommands:
-			fmt.Println("Feed command received in hub for stock :",feed.Symbol)
+		case feed := <-h.FeedCommands:
+			fmt.Println("Feed command received in hub for stock :", feed.Symbol)
 			feedCommands <- feed
 		}
 	}
--- internal/market/binance.go
+++ internal/market/binance.go
@@ -18,25 +18,103 @@ import (
 func StartBinanceMarket(ctx context.Context, redisClient *redis.Client, wg *sync.WaitGroup, feedCommands chan domain.FeedCommand) {
 	defer wg.Done()
 	url := "wss://stream.binance.com:9443/ws"
-	conn, _, err := websocket.DefaultDialer.Dial(url, nil)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	go func() {
-		<-ctx.Done()
-		conn.Close()
-	}()
-	fmt.Println("Binance Connected")
+	activeStocksMap := make(map[string]bool)
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
 
-	go func() {
+		}
+		conn, _, err := websocket.DefaultDialer.Dial(url, nil)
+		if err != nil {
+			select {
+			case <-ctx.Done():
+				return
+			case <-time.After(5 * time.Second):
+			}
+			continue
+		}
+		fmt.Println("Binance Connected")
+		for k, _ := range activeStocksMap {
+			subscribeMsg := map[string]interface{}{
+				"method": "SUBSCRIBE",
+				"params": []string{
+					strings.ToLower(k) + "@trade",
+				},
+				"id": 1,
+			}
+			err = conn.WriteJSON(subscribeMsg)
+			if err != nil {
+				fmt.Println("Resubscribe failed:", err)
+				return
+			}
+
+		}
+		disconnect := make(chan struct{})
+
+		go func() {
+			defer close(disconnect)
+			go func() {
+				time.Sleep(6 * time.Second)
+
+				fmt.Println("FORCING BINANCE DISCONNECT")
+
+				conn.Close()
+			}()
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				default:
+				}
+
+				_, data, err := conn.ReadMessage()
+
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+
+				fmt.Println(string(data))
+				var binanceEvent domain.BinanceEvent
+
+				err = json.Unmarshal(data, &binanceEvent)
+				if err != nil {
+					fmt.Println(err)
+					continue
+				}
+				if binanceEvent.Symbol == "" {
+					continue
+				}
+
+				stockData := binanceEvent.ToStockData()
+
+				fmt.Println(stockData)
+				dataBytes, err := json.Marshal(stockData)
+				if err != nil {
+					fmt.Println("Error marshaling stock data:", err)
+					continue
+				}
+				err = redisClient.Publish(ctx, "stocks", dataBytes).Err()
+				if err != nil {
+					fmt.Println("Error publishing stock data:", err)
+					continue
+				}
+
+			}
+		}()
+
+	connectedLoop:
 		for {
 			select {
 			case <-ctx.Done():
+				conn.Close()
 				return
 			case feed := <-feedCommands:
 				fmt.Println("Feed command received for stock :", feed.Symbol)
 				if feed.Action == "subscribe" {
+					activeStocksMap[feed.Symbol] = true
 					subscribeMsg := map[string]interface{}{
 						"method": "SUBSCRIBE",
 						"params": []string{
@@ -47,9 +125,11 @@ func StartBinanceMarket(ctx context.Context, redisClient *redis.Client, wg *sync
 					err = conn.WriteJSON(subscribeMsg)
 					if err != nil {
 						fmt.Println(err)
-						return
+						conn.Close()
+						break connectedLoop
 					}
 				} else {
+					delete(activeStocksMap, feed.Symbol)
 					unsubscribeMsg := map[string]interface{}{
 						"method": "UNSUBSCRIBE",
 						"params": []string{
@@ -60,45 +140,20 @@ func StartBinanceMarket(ctx context.Context, redisClient *redis.Client, wg *sync
 					err = conn.WriteJSON(unsubscribeMsg)
 					if err != nil {
 						fmt.Println(err)
-						return
+						conn.Close()
+						break connectedLoop
 					}
 				}
+			case <-disconnect:
+				conn.Close()
+				select {
+				case <-time.After(5 * time.Second):
+
+				case <-ctx.Done():
+					return
+				}
+				break connectedLoop
 			}
-		}
-	}()
-	for {
-
-		_, data, err := conn.ReadMessage()
-
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		fmt.Println(string(data))
-		var binanceEvent domain.BinanceEvent
-
-		err = json.Unmarshal(data, &binanceEvent)
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		if binanceEvent.Symbol == "" {
-			continue
-		}
-
-		stockData := binanceEvent.ToStockData()
-
-		fmt.Println(stockData)
-		dataBytes, err := json.Marshal(stockData)
-		if err != nil {
-			fmt.Println("Error marshaling stock data:", err)
-			continue
-		}
-		err = redisClient.Publish(ctx, "stocks", dataBytes).Err()
-		if err != nil {
-			fmt.Println("Error publishing stock data:", err)
-			continue
 		}
 
 	}
