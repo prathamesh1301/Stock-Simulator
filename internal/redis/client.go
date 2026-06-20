6e350be25fbc15eb66bfda56ccf4bcf88105655c
prathamesh1301 <prathameshautade107@gmail.com>
2026-06-20 11:58:37 +0530

    chore: add env

--- internal/redis/client.go
+++ internal/redis/client.go
@@ -1,8 +1,8 @@
 package redis
 
-
 import (
 	"context"
+	"os"
 
 	goredis "github.com/redis/go-redis/v9"
 )
@@ -10,8 +10,11 @@ import (
 var Ctx = context.Background()
 
 func NewClient() *goredis.Client {
-
+	addr := os.Getenv("REDIS_ADDR")
+	if addr == "" {
+		addr = "localhost:6379"
+	}
 	return goredis.NewClient(&goredis.Options{
-		Addr: "localhost:6379",
+		Addr: addr,
 	})
-}
+}
